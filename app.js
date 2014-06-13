927bfa62fb6ce853c46d4c97e36bf0ece7f2614a
Karan Goel <karan@goel.im>
2014-06-13 06:39:40 -0700

    add basic comment scraper

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var Posts = require('./models/posts');
 
 var app = express();
 
-var BASE_URL = 'http://www.producthunt.com/';
+var BASE_URL = 'http://www.producthunt.com';
 
 
-app.configure(function(){
+app.configure(function (){
   app.set('port', process.env.PORT || 8888);
   app.use(express.cookieParser('keyboard cat'));
   app.use(express.session({ secret: 'keyboard cat' }));
@@ -18,22 +18,22 @@ app.configure(function(){
 });
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.redirect('https://github.com/karan/Hook');
 });
 
 
-app.get('/today', function(req, res) {
+app.get('/today', function (req, res) {
 
   var today = new Date().toJSON().slice(0,10);
 
-  Posts.findOne({date: today}, function(err, obj) {
+  Posts.findOne({date: today}, function (err, obj) {
     
     if (obj && obj.expires < Date.now()) {
       // post expired, scrape again, and save
       console.log("posts expired - " + today);
-      scrapeSaveSend(function(posts) {
-        Posts.findOneAndUpdate({date: today}, {posts: posts, expires: new Date(Date.now() + 60*60*1000)}, {new: true}, function(err, newObj) {
+      getPostDetails(null, function (posts) {
+        Posts.findOneAndUpdate({date: today}, {posts: posts, expires: new Date(Date.now() + 60*60*1000)}, {new: true}, function (err, newObj) {
           res.send(200, {
             status: 'success',
             hunts: posts
@@ -51,11 +51,11 @@ app.get('/today', function(req, res) {
       // not in the db, scrape and send
       console.log("posts not found in db - " + today);
 
-      scrapeSaveSend(function(posts) {
+      getPostDetails(null, function (posts) {
         new Posts({
           date: today,
           posts: posts
-        }).save(function(err) {
+        }).save(function (err) {
           res.send(200, {
             status: 'success',
             hunts: posts
@@ -69,27 +69,95 @@ app.get('/today', function(req, res) {
 
 });
 
-function scrapeSaveSend(callback) {
-  var posts = [];
+
+app.get("/posts/:slug", function (req, res) {
+
+  var post_url = '/posts/'+req.params.slug;
+
+  getPostDetails(post_url, function (post) {
+    post = post[0];
+    console.log(post);
+    getComments(post_url, function (err, comments) {
+      res.send(comments);
+    });
+  });
+
+});
+
+
+function getComments(url, callback) {
 
   jsdom.env(
-    BASE_URL,
+    BASE_URL+url,
     ["http://code.jquery.com/jquery.js"],
     function (errors, window) {
       var $ = window.$;
-      var $ph_posts = $(".todays-posts tr");
-      $ph_posts.each(function(rank) {
+
+      var comments = [];
+
+      var comments_dom = $(".modal-container").find(".comment");
+      
+      comments_dom.each(function (index) {
+
+        var name = $(this).find(".comment-user-name a").text();
+        var username = $(this).find(".comment-user-handle").text().match(/\(\@(.*)\)/)[1];
+        var timestamp = $(this).find(".comment-timestamp").text();
+        var comment = $(this).find(".comment-body").find(".comment-user-name").remove().end().text();
+        var comment_html = $(this).find(".comment-body").find(".comment-user-name").remove().end().html();
+
+        comments.push({
+          index: index+1,
+          user: {
+            username: username,
+            name: name
+          },
+          timestamp: timestamp,
+          comment: comment,
+          comment_html: comment_html
+        });
+
+        if (comments.length === comments_dom.length) {
+          callback(null, comments);
+        }
+
+      });
+    }
+  );
+
+}
+
+
+function getPostDetails(post_url, callback) {
+  var url = post_url ? BASE_URL + post_url : BASE_URL;
+  
+  console.log(url);
+
+  var posts = [];
+
+  jsdom.env(
+    url,
+    ["http://code.jquery.com/jquery.js"],
+    function (errors, window) {
+      var $ = window.$;
+
+      var container = post_url ? $(".modal-container") : null;
+      var $ph_posts = post_url ? $(".modal-container .posts-group tr") : $(".todays-posts tr");
+      $ph_posts.each(function (rank) {
 
         var votes = +$(this).find(".upvote").text();
         var name = $(this).find("h3.user-name").clone().children().remove().end().text().trim().replace(/"/g, "");
         var username = $(this).find("span.user-handle").text().trim().replace(/"/g, "").match(/\(\@(.*)\)/)[1];
         var title = $(this).find(".post-url").text();
         var tagline = $(this).find(".post-tagline").text();
-        var comments = $(this).find(".view-discussion").text().trim().match(/(\d+)/g);
-        if (!comments) comments = 0; 
-        else comments = +comments[0];
+        
+        if (container) {
+          var comment_count = $(container.find(".subhead")[2]).text().trim().match(/(\d+)/g);;
+        } else {
+          var comment_count = $(this).find(".view-discussion").text().trim().match(/(\d+)/g);
+        }
+        comment_count = comment_count ? comment_count[0] : 0; 
 
-        var permalink = $(this).find(".view-discussion").attr("data-url");
+        var permalink = post_url ? post_url : $(this).find(".view-discussion").attr("data-url");
         
         request({url: BASE_URL+$(this).find(".post-url").attr("href"), followRedirect: false}, function (error, response, body) {
           url = response.headers.location;
@@ -103,7 +171,7 @@ function scrapeSaveSend(callback) {
             },
             'rank': rank + 1,
             'tagline': tagline,
-            'comments': comments,
+            'comment_count': comment_count,
             'permalink': permalink,
             'url': url
           });
